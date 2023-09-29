955116550502e4096b936c4d06b4d22bcc816ea2
Geon <gunner6603@snu.ac.kr>
2023-09-29 21:49:59 +0900

    feat: BaseTimeEntity 어노테이션 추가 Jackson 라이브러리가 LocalDateTime 역직렬화 못하는 이슈 해결

--- src/main/java/com/gugucon/shopping/common/domain/entity/BaseTimeEntity.java
+++ src/main/java/com/gugucon/shopping/common/domain/entity/BaseTimeEntity.java
@@ -1,5 +1,9 @@
 package com.gugucon.shopping.common.domain.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -20,8 +24,12 @@ public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime lastModifiedAt;
 }
